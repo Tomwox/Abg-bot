b6ca1511482ec0e6f2335a9431dfbcb5e7e963a6
thomas.lemire123 <thomas.lemire123@icloud.com>
2021-05-27 15:26:49 -0400

    deleted facebook messenger verification

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -16,11 +16,6 @@ get '/' do
   end
 end
 
-Facebook::Messenger::Subscriptions.subscribe(
-  access_token: ENV["ACCESS_TOKEN"],
-  subscribed_fields: %w[messages, messaging_postbacks]
-)
-
 Faceboook::Messenger::Bot.on :message do |message|
   message.reply(text: 'Hello, human!')
 end
