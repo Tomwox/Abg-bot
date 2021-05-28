467e0987689758d34eca4df8202ff1a634683122
thomas.lemire123 <thomas.lemire123@icloud.com>
2021-05-28 15:02:15 -0400

    other try

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -4,16 +4,6 @@ include Facebook::Messenger
 
 VERIFY_TOKEN = 'loulou'
 
-get '/' do
-  mode = params['hub.mode']
-  verify_token = params['hub.verify_token']
-  challenge = params['hub.challenge']
-  if mode == 'subscribe' && verify_token == VERIFY_TOKEN
-    challenge
-  else
-    halt 401
-  end
-  
 Facebook::Messenger::Subscriptions.subscribe(
   access_token: ENV["ACCESS_TOKEN"],
   subscribed_fields: %w[messages, messaging_postbacks]
