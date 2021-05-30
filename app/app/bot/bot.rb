15049932e608de3fe77c494b4dfb2e810e7e3b50
thomas.lemire123 <thomas.lemire123@icloud.com>
2021-05-30 16:36:36 -0400

    puts

--- app/app/bot/bot.rb
+++ app/app/bot/bot.rb
@@ -3,11 +3,16 @@ require 'facebook/messenger'
 include Facebook::Messenger
 
 VERIFY_TOKEN = ENV["VERIFY_TOKEN"]
+puts VERIFY_TOKEN
+
+APP_SECRET = ENV["APP_SECRET"]
+puts APP_SECRET
 
 Facebook::Messenger::Subscriptions.subscribe(
   access_token: ENV["ACCESS_TOKEN"],
   subscribed_fields: %w[messages messaging_postbacks]
 )
+puts ACCESS_TOKEN
 
 Facebook::Messenger::Bot.on :message do |message|
   message.reply(text: 'Hello, human!')
