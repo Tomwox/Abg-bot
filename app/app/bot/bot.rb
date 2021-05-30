e70ca49956d1566c7aee0a6c626c84c6817ec354
thomas.lemire123 <thomas.lemire123@icloud.com>
2021-05-30 16:42:08 -0400

    access token

--- app/app/bot/bot.rb
+++ app/app/bot/bot.rb
@@ -8,11 +8,13 @@ puts VERIFY_TOKEN
 APP_SECRET = ENV["APP_SECRET"]
 puts APP_SECRET
 
+ACCESS_TOKEN = ENV["ACCESS_TOKEN"]
+puts ACCESS_TOKEN
+
 Facebook::Messenger::Subscriptions.subscribe(
   access_token: ENV["ACCESS_TOKEN"],
   subscribed_fields: %w[messages messaging_postbacks]
 )
-puts ACCESS_TOKEN
 
 Facebook::Messenger::Bot.on :message do |message|
   message.reply(text: 'Hello, human!')
