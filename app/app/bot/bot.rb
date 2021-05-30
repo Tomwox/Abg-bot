19dd5656fb104188a46c94d41c82fb0a9e66e4bb
thomas.lemire123 <thomas.lemire123@icloud.com>
2021-05-30 16:04:55 -0400

    pg

--- app/app/bot/bot.rb
+++ app/app/bot/bot.rb
@@ -2,7 +2,7 @@
 require 'facebook/messenger'
 include Facebook::Messenger
 
-VERIFY_TOKEN = 'loulou'
+VERIFY_TOKEN = ENV["VERIFY_TOKEN"]
 
 Facebook::Messenger::Subscriptions.subscribe(
   access_token: ENV["ACCESS_TOKEN"],
